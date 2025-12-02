e32e84e128e75e4dc7210e95e9ffa0f002db856b
rathish880 <rathishkumar946@gmail.com>
2025-12-02 22:28:40 +0530

    Ceiling of a number in array -02/12/2025

---
+++ Concepts/ceililng_of__target_element.java
@@ -0,0 +1,27 @@
+package Concepts;
+
+public class ceililng_of__target_element {
+
+    public static void main (String args[]){
+        int[] arr={1,3,7,9,10,15,19};
+        int ceiling=ceiling(arr,0,arr.length-1,20);
+        System.out.println(ceiling);
+    }
+    public static int ceiling(int arr[],int start,int end,int target){
+        // int start=start;
+        // int end =end;
+        
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target)
+                return arr[mid];
+            else if(arr[mid]>target)
+                end=mid-1;
+            else
+                start=mid+1;
+        }
+        return arr[start];
+    }
+
+    
+}
